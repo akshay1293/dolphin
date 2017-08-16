be83e556c62d45159e03bbcb9a096f2f303b26b6
akshay1293 <akshayaggarwal1293@gmail.com>
2017-08-16 12:02:24 +0530

    added google sign in

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    window.gapi.signin2.render('signin-button', {
+      'scope': 'https://www.googleapis.com/auth/plus.login',
+      'longtitle': false,
+      'theme': 'light',
+      'height': 60,
+      'width': 180,
+      'onsuccess': this.onSignIn.bind(this),
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div id="signin-button" />
       </div>
     );
+  }
+
+  onSignIn(user) {
+    console.log(user);
   }
 }
 
