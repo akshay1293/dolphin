e2b24c9bc96124f09035cda5515a4b69f83b21be
akshay1293 <akshayaggarwal1293@gmail.com>
2017-08-18 13:30:57 +0530

    sign out functionallity added

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import cookies from 'universal-cookie';
+
 
 export default class Dashboard extends Component {
 
+    constructor(props) {
+
+        super(props);
+        this.cookie = new cookies();
+    }
+
     render() {
 
-        return <div>dashboard</div>
+        return <div><a href="#" onClick={this.props.signOut}>signout</a></div>
     }
+
 }
