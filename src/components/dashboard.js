9d2c05a2c51c3af41e83f4699dd330f5530edae3
jaghit <iamjaghitsingh@gmail.com>
2017-08-21 18:33:46 +0530

    SIDEBAR

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import Button from './button';
-
+import SideBar from './sideBar.js';
 
 export default class Dashboard extends Component {
     render() {
         return (
-            <div>
-                <Button text='SignOut' color='#7ad5c9' height='50px' width='80px' onClick={this.props.signOut} />
+            <div style={dolphin.container}>
+                <div style={dolphin.sidebar}>
+                    <SideBar />
+                </div>
+                <div style={dolphin.content}>
+                    <Button text='SignOut' width='80px' onClick={this.props.signOut} />
+                </div>
             </div>
         );
     }
 }
+
+const dolphin = {
+    container: {
+        bottom: 0,
+        display: 'flex',
+        left: 0,
+        position: 'absolute',
+        right: 0,
+        top: 0
+    },
+    sidebar: {
+        display: 'flex',
+        flex: '0.18'
+    },
+    content: {
+        flex: '0.82'
+    }
+};
---
+++ src/components/sideBar.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import Dolphin from '../assets/dolphin-logo.png';
+
+class SideBar extends React.Component {
+    uploadFile() {
+        console.log('uploadFile; testOk');
+    }
+    createFolder() {
+        console.log('createFolder; testOk');
+    }
+    render() {
+        return (
+            <div style={dolphin.global}>
+                <img style={dolphin.image} src={Dolphin} alt='Dolphin' />
+                <hr style={dolphin.seperator} />
+                <Button name='Upload File' func={this.uploadFile} class='fa fa-upload' />
+                <Button name='Create Folder' func={this.createFolder} class='fa fa-plus-square' />
+                <span style={{ position: 'absolute', bottom: '16px' }}>All rights reserved.</span>
+            </div>
+        );
+    }
+}
+
+class Button extends React.Component {
+    render() {
+        return (
+            <span style={dolphin.button} onClick={this.props.func}>
+                <i style={dolphin.font} className={this.props.class} aria-hidden="true"></i>
+                {this.props.name}
+            </span>
+        );
+    }
+}
+
+const dolphin = {
+    global: {
+        alignItems: 'center',
+        backgroundColor: '#34495E',
+        color: '#FFFFFF',
+        display: 'flex',
+        flexDirection: 'column',
+        position: 'relative',
+        width: '100%'
+    },
+    image: {
+        height: '139px',
+        padding: '32px 0 0 0',
+        width: '168px'
+    },
+    seperator: {
+        border: '1px solid #7AD5C9',
+        margin: '32px 0 24px 0',
+        width: '100%'
+    },
+    button: {
+        alignItems: 'center',
+        border: '2px solid #AAA',
+        borderRadius: '6px',
+        color: '#EEE',
+        cursor: 'pointer',
+        display: 'flex',
+        fontFamily: 'sans-serif',
+        fontSize: '18px',
+        height: '36px',
+        justifyContent: 'center',
+        margin: '8px',
+        width: '168px'
+    },
+    font: {
+        color: '#7AD5C9',
+        marginRight: '8px'
+    }
+};
+
+export default SideBar;
